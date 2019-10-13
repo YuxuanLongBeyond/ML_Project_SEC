d5c76dbaccbd80942c52b59cceb97aa128e8db19
YuxuanLongBeyond <yuxuan.long@epfl.ch>
2019-10-13 11:07:36 +0200

    Update training

--- test.py
+++ test.py
@@ -5,19 +5,73 @@ Created on Thu Sep 26 20:10:29 2019
 
 @author: YuxuanLong
 """
-# show prediction during training
 
-test_num = 0
-pred_np = var_to_np(pred)[test_num][0]
 
-new_mask = pred_np >= 0.5
+#test_num = 0
+#pred_np = var_to_np(pred)[test_num][0]
+#
+#new_mask = pred_np >= 0.5
+#
+#image_up = var_to_np(image)[0]
+#
+#image_new = np.moveaxis(image_up, 0, 2)
+#
+#channel = image_new[:, :, 0]
+#channel[new_mask] = 1.0
+#image_new[:, :, 0] = channel
+#
+#io.imshow(image_new)
 
-image_up = var_to_np(image)[0]
 
-image_new = np.moveaxis(image_up, 0, 2)
 
-channel = image_new[:, :, 0]
-channel[new_mask] = 1.0
-image_new[:, :, 0] = channel
+import os
+import shutil
+# import argparse
+import random
+import numpy as np
+import time
 
-io.imshow(image_new)
+from skimage import io, transform
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import torchvision.utils
+
+import torch.utils.data as utils_data
+
+import utils
+
+if __name__ == '__main__':
+    test_dir = './data/main_data/test'
+    
+    net = utils.create_models()
+    net.load_state_dict(torch.load('./parameters/weights'))
+    net.eval()
+    
+    resize = False
+    image_size = 384
+        ##create single image tensor for test in each epoch
+    test_image_origin = io.imread('./data/main_data/test/test_01.png')
+    test_image_origin = utils.image_transform(test_image_origin, resize, image_size)
+    test_image_dum = np.moveaxis(test_image_origin, 0, 2)
+    test_image = np.expand_dims(test_image_origin, axis = 0)
+    test_image = utils.np_to_var(torch.from_numpy(test_image))  
+    
+    
+    
+        ###dummy test
+    pred_test = net.forward(test_image)
+    pred_np = utils.var_to_np(pred_test)[0][0]
+    
+    new_mask = pred_np >= 0.5
+    
+    dummy = test_image_dum + 0
+    
+    channel = dummy[:, :, 0]
+    channel[new_mask] = 1.0
+    dummy[:, :, 0] = channel
+        
+    dummy = (dummy * 255).astype(np.uint8)
+    io.imsave('./output/test1' + '.png', dummy)
--- train.py
+++ train.py
@@ -12,28 +12,20 @@ Created on Thu Sep 26 20:07:35 2019
 import os
 import shutil
 # import argparse
-import random
+from skimage import io
 import numpy as np
 import time
 
-from skimage import io, transform
-
 import torch
-import torch.nn as nn
 import torch.optim as optim
-from torch.autograd import Variable
-import torchvision.utils
 
-import torch.utils.data as utils_data
-
-import model
+import utils
 
 
 # Run on GPU if CUDA is available.
 RUN_ON_GPU = torch.cuda.is_available()
 
 SEED = 0
-ROOT = './data/main_data/training'
 
 np.random.seed(SEED)
 torch.manual_seed(SEED)
@@ -41,95 +33,18 @@ if RUN_ON_GPU:
     torch.cuda.manual_seed(SEED)
 
 
-def np_to_var(x):
-    """Converts numpy to variable."""
-    if RUN_ON_GPU:
-        x = x.cuda()
-    return Variable(x)
-
-
-def var_to_np(x):
-    """Converts variable to numpy."""
-    if RUN_ON_GPU:
-        x = x.cpu()
-    return x.data.numpy()
-
-def image_transform(image, resize, size):
-    if resize:
-        image = transform.resize(image, (size, size), mode = 'constant', anti_aliasing=True)
-    image = np.array(image)
-    image = np.moveaxis(image, 2, 0) # tensor format
-    image = image.astype(np.float32)
-    return image
-        
-def mask_transform(mask, resize, size):
-    if resize:
-        mask = transform.resize(mask, (size, size), mode = 'constant', anti_aliasing=True)
-    mask = np.array(mask >= 0.5).astype(np.float32) # test here
-    mask = np.expand_dims(mask, axis = 0)
-    return mask
-
-class MyDataset(utils_data.Dataset):
-    def __init__(self, resize = None, size = 384):
-        self.size = size
-        mask_dir = ROOT + '/groundtruth'
-        self.resize = resize
-        self.mask_file_list = [f for f in os.listdir(mask_dir) if os.path.isfile(os.path.join(mask_dir, f))]
-        random.shuffle(self.mask_file_list)
-
-    def __getitem__(self, index):
-        file_name =  self.mask_file_list[index].rsplit('.', 1)[0]
-        img_name = ROOT + '/images/' + file_name+'.png'
-        mask_name = ROOT + '/groundtruth/' + file_name+'.png'
-        
-        image = io.imread(img_name)
-        mask = io.imread(mask_name)
-        
-        image = image_transform(image, self.resize, self.size)
-        mask = mask_transform(mask, self.resize, self.size)
-        
-        sample = {'image': image, 'mask': mask}
-
-        return sample
-  
-    def __len__(self):
-        return len(self.mask_file_list)
-
-
-def get_data_loader(resize = True, image_size = 384, batch_size=10):
-    """Creates training data loader."""
-    train_dataset = MyDataset(resize, image_size)
-    return utils_data.DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle=True)
-
-
-def create_models():
-
-    net = model.LinkNet()
-
-    if RUN_ON_GPU:
-        print('Moving models to GPU.')
-        net.cuda()
-    else:
-        print('Keeping models on CPU.')
-
-    return net
-
-
-
-    
-    
-
 if __name__ == '__main__':
+    root = './data/main_data/training'
     if os.path.exists('./epoch_output'):
         shutil.rmtree('./epoch_output')
     os.makedirs('./epoch_output')
-    net = create_models()
+    net = utils.create_models()
     image_size = 384
-    batch_size = 20
+    batch_size = 1
     num_epochs = 1
     save_interval = 1
-    out_dir = 'output'
-    test_image_name = ROOT + '/images/satImage_001.png'
+
+    test_image_name = root + '/images/satImage_001.png'
     resize = True
     
     lr = 2e-4
@@ -140,21 +55,19 @@ if __name__ == '__main__':
     
     ##create single image tensor for test in each epoch
     test_image_origin = io.imread(test_image_name)
-    test_image_origin = image_transform(test_image_origin, resize, image_size)
+    test_image_origin = utils.image_transform(test_image_origin, resize, image_size)
     test_image_dum = np.moveaxis(test_image_origin, 0, 2)
     test_image = np.expand_dims(test_image_origin, axis = 0)
-    test_image = np_to_var(torch.from_numpy(test_image))    
+    test_image = utils.np_to_var(torch.from_numpy(test_image))    
     
     
     
     # create optimizers
     optimizer = optim.Adam(net.parameters(), lr = lr, weight_decay = weight_decay, amsgrad = True)
-    Loss = model.Loss(smooth, lam, beta)
-    # create output directory for image samples
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
+    Loss = utils.loss(smooth, lam, beta)
 
-    dataloader = get_data_loader(resize, image_size = image_size, batch_size = batch_size)
+
+    dataloader = utils.get_data_loader(root, resize, image_size = image_size, batch_size = batch_size)
     num_batch = len(dataloader)
     total_train_iters = num_epochs * num_batch
 
@@ -166,8 +79,8 @@ if __name__ == '__main__':
         epoch_loss = 0 
         for iteration, batch in enumerate(dataloader, epoch * num_batch + 1):
             print('Iteration: ', iteration)
-            image = np_to_var(batch['image'])
-            mask = np_to_var(batch['mask'])
+            image = utils.np_to_var(batch['image'])
+            mask = utils.np_to_var(batch['mask'])
 #
             optimizer.zero_grad()
 
@@ -186,19 +99,11 @@ if __name__ == '__main__':
                 iteration, total_train_iters, loss.data.item()))
             
             # keep track of loss for plotting and saving
-            loss_history.append(loss.data.item())
-#
-##            # save the generated samples and loss
-#            if iteration % save_interval == 0:
-#
-#                # save the loss history
-#                with open('loss.txt', 'wt') as file:
-#                    file.write('\n'.join(['{}'.format(loss) for loss in loss_history]))
-#                    file.write('\n')
+
             
         ###dummy test
         pred_test = net.forward(test_image)
-        pred_np = var_to_np(pred_test)[0][0]
+        pred_np = utils.var_to_np(pred_test)[0][0]
         
         new_mask = pred_np >= 0.5
         
@@ -215,5 +120,9 @@ if __name__ == '__main__':
         print('In the epoch ', epoch, ', the average loss is ', epoch_loss)
         
         
-        
+    torch.save(net.state_dict(), './parameters/weights')
+    # save the loss history
+    with open('loss.txt', 'wt') as file:
+        file.write('\n'.join(['{}'.format(loss) for loss in loss_history]))
+        file.write('\n')
 
