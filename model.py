f58f12bdc9850384b699046a41ab4be9c7a245c3
YuxuanLongBeyond <yuxuan.long@epfl.ch>
2019-11-27 21:08:23 +0100

    update Dlink + model

--- model.py
+++ model.py
@@ -138,8 +138,9 @@ class D_plus_LinkNet(nn.Module):
         self.conv1 = nn.Sequential(*[nn.Conv2d(256, 256, kernel_size = 3, padding = 1), nn.BatchNorm2d(256), nn.ReLU()])
         self.conv2 = nn.Sequential(*[nn.Conv2d(128, 128, kernel_size = 3, padding = 1), nn.BatchNorm2d(128), nn.ReLU()])
         self.conv3 = nn.Sequential(*[nn.Conv2d(64, 64, kernel_size = 3, padding = 1), nn.BatchNorm2d(64), nn.ReLU()])
-        self.conv4 = nn.Sequential(*[nn.Conv2d(64, 64, kernel_size = 7, padding = 3), nn.BatchNorm2d(64), nn.ReLU()])
-        
+#        self.conv4 = nn.Sequential(*[nn.Conv2d(64, 64, kernel_size = 7, padding = 3), nn.BatchNorm2d(64), nn.ReLU()])
+        self.conv4 = nn.Sequential(*[nn.Conv2d(64, 64, kernel_size = 3, padding = 1), nn.BatchNorm2d(64), nn.ReLU(), 
+                                     nn.Conv2d(64, 64, kernel_size = 3, padding = 1), nn.BatchNorm2d(64), nn.ReLU()])
         
         decoder5 = [nn.ConvTranspose2d(64, 32, kernel_size = 4, stride = 2, padding = 1), 
                     nn.BatchNorm2d(32), nn.ReLU(), nn.Conv2d(32, 32, kernel_size = 3, padding = 1), 
@@ -218,6 +219,9 @@ class Loss(nn.Module):
 #        return torch.mean(bce)
         loss = nn.BCELoss()
         return loss(pred, mask)
+#    
+#    def focal_loss(self, pred, mask):
+#        
         
         
     def dice_loss(self, pred, mask):
