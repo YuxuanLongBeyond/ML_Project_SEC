fix numerical issue for focal loss

model.py
@@ -222,7 +222,7 @@ class Loss(nn.Module):
         loss = nn.BCELoss()
         return loss(pred, mask)
     
-    def focal_loss(self, pred, mask, epsilon = 1e-20):
+    def focal_loss(self, pred, mask, epsilon = 1e-6):
         pred = torch.clamp(pred, min = epsilon, max = 1.0 - epsilon)
         loss = - mask * torch.pow(1.0 - pred, self.gamma) * torch.log(pred) - (1.0 - mask) * torch.pow(pred, self.gamma) * torch.log(1.0 - pred)
         return torch.mean(loss)

train.py
@@ -160,10 +160,10 @@ if __name__ == '__main__':
                 for param_group in optimizer.param_groups:
                     param_group['lr'] = lr     
         
-        if loss_type == 'focal' and (epoch + 1) % 100 == 0:
-            gamma += gamma_increment
-            with torch.no_grad():
-                Loss = utils.loss(smooth, lam, gamma, loss_type)
+#        if loss_type == 'focal' and (epoch + 1) % 100 == 0:
+#            gamma += gamma_increment
+#            with torch.no_grad():
+#                Loss = utils.loss(smooth, lam, gamma, loss_type)
         
         epoch_loss /= num_batch
         print('In the epoch ', epoch, ', the average batch loss is ', epoch_loss)